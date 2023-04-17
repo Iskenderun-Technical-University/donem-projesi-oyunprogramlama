4b02940e378b3975c2f21d2e4e122de0b47d51c2
Kutay Tıstık <kutaytistik99@gmail.com>
2023-04-17 22:09:31 +0300

    a

--- Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
+++ Assets/Standard Assets/Utility/SimpleActivatorMenu.cs
@@ -8,7 +8,7 @@ namespace UnityStandardAssets.Utility
     {
         // An incredibly simple menu which, when given references
         // to gameobjects in the scene
-   
+        
         public GameObject[] objects;
 
 
@@ -33,7 +33,7 @@ namespace UnityStandardAssets.Utility
             }
 
             m_CurrentActiveObject = nextactiveobject;
-            
+           
         }
     }
 }
